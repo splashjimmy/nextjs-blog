6a024b08f6195744fd8a995b787da3fa22992e4b
Jimmy Huang <jimmy.huang@splashthat.com>
2021-10-01 15:49:23 -0400

    fix socket connection state

--- pages/test.js
+++ pages/test.js
@@ -3,18 +3,24 @@ import io from "socket.io-client";
 
 export default function Test() {
   const [socket, setSocket] = useState(null);
+  const [isSocketConnected, setIsSocketConnected] = useState(false);
 
   useEffect(() => {
     const newSocket = io(`http://${window.location.hostname}:3000`);
-    console.log(newSocket);
     setSocket(newSocket);
+
+    newSocket.on("connect", function () {
+      setIsSocketConnected(newSocket.connected);
+      newSocket.emit("front-end initial emit");
+    });
+
     return () => newSocket.close();
   }, [setSocket]);
 
   return (
     <>
       <h1>Connection Status</h1>
-      {socket?.connected ? (
+      {isSocketConnected ? (
         <div className="chat-container">
           <div>Connected socket</div>
         </div>
